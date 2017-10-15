2052178d23c6c5ddcb22e00c10fb417531d53197
Andrey Frazatti Alves <desenvolvedorcsharp@outlook.com>
2017-10-15 19:13:05 -0200

    Documentação Javadoc
    Documentado a classe DAOFactory.

--- Fontes/ProjetoHealthTrack/src/br/com/healthtrack/dao/DAOFactory.java
+++ Fontes/ProjetoHealthTrack/src/br/com/healthtrack/dao/DAOFactory.java
@@ -2,32 +2,66 @@ package br.com.healthtrack.dao;
 
 import br.com.healthtrack.dao.interfaces.*;
 
+/***
+ * Classe responsáve por criar as instâncias das DAOs.
+ * @author Andrey Frazatti Alves
+ * @version 1.0
+ *
+ */
 public final class DAOFactory {
 	
+	/***
+	 * Retorna uma instância de DAO para alimento consumido.
+	 * @return Instância de DAO.
+	 */
 	public static AlimentoConsumidoDAO getAlimentoConsumidoDAO() {
 		return new OracleAlimentoConsumidoDAO();
 	}
 	
+	/***
+	 * Retorna uma instância de DAO para atividade física.
+	 * @return Instância de DAO.
+	 */
 	public static AtividadeFisicaDAO getAtividadeFisicaDAO() {
 		return new OracleAtividadeFisicaDAO();
 	}
 	
+	/***
+	 * Retorna uma instância de DAO para peso.
+	 * @return Instância de DAO.
+	 */
 	public static PesoDAO getPesoDAO() {
 		return new OraclePesoDAO();
 	}
 	
+	/***
+	 * Retorna uma instância de DAO para pressão arterial.
+	 * @return Instância de DAO.
+	 */
 	public static PressaoArterialDAO getPressaoArterialDAO() {
 		return new OraclePressaoArterialDAO();
 	}
 	
+	/***
+	 * Retorna uma instância de DAO para tipo de atividade física.
+	 * @return Instância de DAO.
+	 */
 	public static TipoAtividadeFisicaDAO getTipoAtividadeFisicaDAO() {
 		return new OracleTipoAtividadeFisicaDAO();
 	}
 	
+	/***
+	 * Retorna uma instância de DAO para tipo de refeição.
+	 * @return Instância de DAO.
+	 */
 	public static TipoRefeicaoDAO getTipoRefeicaoDAO() {
 		return new OracleTipoRefeicaoDAO();
 	}
 	
+	/***
+	 * Retorna uma instância de DAO para usuário.
+	 * @return Instância de DAO.
+	 */
 	public static UsuarioDAO getUsuarioDAO() {
 		return new OracleUsuarioDAO();
 	}
